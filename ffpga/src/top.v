3c2aa7d5168fc9bc38de19bd58dce8c62de878ad
AkshatSharma05 <d06akshat@gmail.com>
2026-07-18 00:45:04 +0530

    unverified vga timing module

--- ffpga/src/top.v
+++ ffpga/src/top.v
@@ -2,7 +2,7 @@
 // Vicharak's Blinky Demo
 // Signals must match io_map.pcf assignments.
 
-(* top *) module vga(
+(* top *) module top(
   (* iopad_external_pin, clkbuf_inhibit *) input clk,
   (* iopad_external_pin *) input rst,
 
@@ -25,6 +25,30 @@
     .tick_25MHZ  (tick_25MHZ),
     .rst         (reset),
     .LED         (LED)
+  );
+
+  wire [9:0] hcount;
+  wire [9:0] vcount;
+  wire hsync;
+  wire vsync;
+  wire video_active;
+  wire frame_start;
+  wire line_start;
+
+  vga_timing u_vga_timing (
+      .clk(clk),
+      .rst(reset),
+      .tick_25MHZ(tick_25MHZ),
+
+      .hcount(hcount),
+      .vcount(vcount),
+
+      .hsync(hsync),
+      .vsync(vsync),
+      .video_active(video_active),
+
+      .frame_start(frame_start),
+      .line_start(line_start)
   );
 
   assign LED_en = 1'b1;
--- ffpga/src/vga_timing.v
+++ ffpga/src/vga_timing.v
@@ -30,27 +30,63 @@
 */
 
 module vga_timing(
+    //all signals are wire if not specified
     input clk,
     input rst,
     input tick_25MHZ,
 
-    output hcount,
-    output vcount,
+    //use reg when signals have to be directly assigned in an always block
+    output reg [9:0] hcount,
+    output reg [9:0] vcount,
+
     output hsync,
     output vsync,
-    output video_active,
+
+    output video_active,  // high when signals are in the visible range
     output frame_start,
     output line_start
 );
+    localparam H_VISIBLE = 640;
+    localparam H_FRONT   = 16;
+    localparam H_SYNC    = 96;
+    localparam H_BACK    = 48;
+    localparam H_TOTAL   = 800;
+
+    localparam V_VISIBLE = 480;
+    localparam V_FRONT   = 10;
+    localparam V_SYNC    = 2;
+    localparam V_BACK    = 33;
+    localparam V_TOTAL   = 525;
 
     reg [31:0] counter = 32'd0;
 
     always @ (posedge clk or negedge rst) begin
         if (!rst) begin
             counter <= 32'd0;
+            hcount <= 0;
+            vcount <= 0;
         end else if (tick_25MHZ) begin
-            /**/
+            if (hcount == H_TOTAL - 1) begin
+                hcount <= 0;
+
+                if (vcount == V_TOTAL - 1)
+                    vcount <= 0;
+                else
+                    vcount <= vcount + 1;
+            end
+            else
+                hcount <= hcount + 1;
         end
     end
 
+    assign video_active = (hcount < H_VISIBLE) && (vcount < V_VISIBLE);
+
+    assign hsync = ~((hcount >= H_VISIBLE + H_FRONT) && (hcount <  H_VISIBLE + H_FRONT + H_SYNC));
+    assign vsync = ~((vcount >= V_VISIBLE + V_FRONT) && (vcount < V_VISIBLE + V_FRONT + V_SYNC));
+
+    assign frame_start = (hcount == 0) &&
+                         (vcount == 0) && 
+                         tick_25MHZ;
+
+    assign line_start = (hcount == 0) && tick_25MHZ;                  
 endmodule 
